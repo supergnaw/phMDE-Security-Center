cef97ea13a94223ec9b75e32267cdec32fc02fc7
Loren Supernaw <loren.supernaw@gmail.com>
2023-08-14 13:56:18 -0500

    added new endpoints to _consts file

--- mdesecuritycenter_consts.py
+++ mdesecuritycenter_consts.py
@@ -11,4 +11,57 @@
 # either expressed or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 
-# Define your constants here
+"""
+    INCIDENTS
+"""
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender/api-incident
+INCIDENT_LIST = "/api/incidents"
+INCIDENT_SINGLE = "/api/incidents/{incident_id}"
+
+"""
+    ALERTS
+"""
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/alerts
+ALERT_LIST = "/api/alerts"
+ALERT_SINGLE = "/api/alerts/{alert_id}"
+ALERT_BATCH_UPDATE = "/api/alerts/batchUpdate"
+ALERT_CREATE = "/api/alerts/CreateAlertByReference"
+ALERT_FILES = "/api/alerts/{alert_id}/files"
+
+"""
+    LIVE RESPONSE
+"""
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/list-library-files
+LIVE_RESPONSE_LIST_LIBRARY = "/api/libraryfiles"
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/run-live-response
+LIVE_RESPONSE_RUN_SCRIPT = "/api/machines/{machine_id}/runliveresponse"
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/get-live-response-result
+LIVE_RESPONSE_GET_RESULT = "/api/machineactions/{action_id}/GetLiveResponseResultDownloadLink(index={command-index})"
+
+"""
+    INVESTIGATIONS
+"""
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/investigation
+INVESTIGATION_LIST = "/api/investigations"
+INVESTIGATION_SINGLE = "/api/investigations/{investigation_id}"
+INVESTIGATION_START = "/api/machines/{machine_id}/startInvestigation"
+
+"""
+    MACHINES
+"""
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/get-machineaction-object
+MACHINE_LIST_ACTIONS = "/api/machineactions/{machine_id}"
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/collect-investigation-package
+MACHINE_COLLECT_INVESTIGATION_PACKAGE = "/api/machines/{id}/collectInvestigationPackage"
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/isolate-machine
+MACHINE_ISOLATE = "/api/machines/{machine_id}/isolate"
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/unisolate-machine
+MACHINE_UNISOLATE = "/api/machines/{machine_id}/unisolate"
+
+"""
+    FILES
+"""
+# https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/stop-and-quarantine-file
+FILE_QUARANTINE = "/api/machines/{machine_id}/StopAndQuarantineFile"
+FILE_INFO = "/api/files/{file_id}"
+FILE_STATS = "/api/files/{file_id}/stats"
